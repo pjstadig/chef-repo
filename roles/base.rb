3b1b071afd74099c1c6d7bfba1ba7f46bf1e6463
Paul Stadig <paul@stadig.name>
2011-03-17 08:37:31 -0400

    Import runit version 0.14.2

--- roles/base.rb
+++ roles/base.rb
@@ -1,8 +1,8 @@
 name "base"
 description "Base role applied to all nodes."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("recipe[ohai]")
+run_list("recipe[ohai]", "recipe[chef::client]")
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
-#override_attributes()
+#override_attributes("chef" => {"server_url" => "https://api.opscode.com/organizations/stadig"})
