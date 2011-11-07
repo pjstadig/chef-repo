2a606213e843eb6324dbbfa70d3dd64aab9be1f0
Paul Stadig <paul@stadig.name>
2011-11-07 08:43:29 -0500

    Setup users.

--- roles/base.rb
+++ roles/base.rb
@@ -1,8 +1,13 @@
 name "base"
 description "Base role applied to all nodes."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("recipe[ohai]", "recipe[chef::client_service]")
+run_list("recipe[ohai]", "recipe[chef::client_service]", "recipe[users::sysadmins]", "recipe[sudo]")
 # Attributes applied if the node doesn't have it set already.
-#default_attributes()
+default_attributes "authorization" => {
+  "sudo" => {
+    "passwordless" => true
+  }
+}
+
 # Attributes applied no matter what the node has set already.
 #override_attributes()
