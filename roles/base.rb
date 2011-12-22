a5f8415b3139444f4f909505bd53e32864152a15
Paul Stadig <paul@stadig.name>
2011-12-21 22:01:36 -0500

    Update for new chef cookbook.

--- roles/base.rb
+++ roles/base.rb
@@ -1,7 +1,7 @@
 name "base"
 description "Base role applied to all nodes."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("recipe[ohai]", "recipe[chef::client_service]", "recipe[users::sysadmins]", "recipe[sudo]")
+run_list("recipe[ohai]", "recipe[chef-client::config]", "recipe[users::sysadmins]", "recipe[sudo]")
 # Attributes applied if the node doesn't have it set already.
 default_attributes "authorization" => {
   "sudo" => {
