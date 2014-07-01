71142f80d0b70a3b4bc7ca3ad9bfc02d7844a509
Paul Stadig <paul@stadig.name>
2014-07-01 16:32:09 -0400

    Verify SSL connections.

--- config/solo.rb
+++ config/solo.rb
@@ -3,3 +3,4 @@ CHEF_REPO = File.expand_path(File.dirname(__FILE__) + "/..")
 file_cache_path "#{CHEF_REPO}/.solo"
 cookbook_path   "#{CHEF_REPO}/.solo/cookbooks"
 role_path       "#{CHEF_REPO}/.solo/roles"
+ssl_verify_mode :verify_peer
